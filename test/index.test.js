89b9ac58b50058610a7e755281dfcf4a0c2c9e63
igorskyflyer <igor.dvlpr@gmail.com>
2021-08-09 15:52:50 +0200

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
-import { assert as chai } from 'chai'
-import { platform } from 'os'
-import { pathExists, pathExistsUnix, pathExistsWindows } from '../src/main.js'
+const chai = require('chai').assert
+const { platform } = require('os')
+const { pathExists, pathExistsUnix, pathExistsWindows } = require('../src/index.js')
 
 const os = platform()
 const isWindows = os === 'win32'
